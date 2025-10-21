e5dc65a2a445539ab3878fe1256a0f188db6471c
unknown <richardkwabenaobeng17@gmail.com>
2025-10-21 03:16:30 +0000

    src updated

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import dill
 
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 
 def save_object(file_path, obj):
@@ -29,19 +30,24 @@ def load_object(file_path):
 def evaluate_models(X_train,X_test,y_train,y_test,models):
 	try:
 		report = {}
-		for i in range(len(list(models))):
-			model = list(models.values())[i]
-			
-            model.fit(X_train,y_train)
+		# models is expected to be a dict: {name: estimator}
+		for name, model in models.items():
+			# train
+			model.fit(X_train, y_train)
 
+			# predict
+			y_train_pred = model.predict(X_train)
+			y_test_pred = model.predict(X_test)
 
-            y_test_pred = model.predict(X_test)
+			# evaluate
+			train_model_score = r2_score(y_train, y_train_pred)
+			test_model_score = r2_score(y_test, y_test_pred)
 
-            train_model_score = r2_score(y_train,y_train_pred)
+			report[name] = {
+				'train_r2': float(train_model_score),
+				'test_r2': float(test_model_score)
+			}
 
-            train_model_score = r2_score(y_test,y_test_pred)
-	
-            report[list(models.keys())[i]] = test_model_score
-        return report
-    except Exception as e:
-        raise CustomException(e,sys)
+		return report
+	except Exception as e:
+		raise CustomException(e, sys)
